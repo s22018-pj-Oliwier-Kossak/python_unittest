fdfd735f65fb3f01d2879fe5ed1d73cfe4cf6526
Oliwier Kossak <oliwierkossak@wp.pl>
2023-04-28 20:40:56 +0200

    test fixtures

---
+++ 05_test_fixtures/01_module_level.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+def setUpModule():
+    print("set up module")
+
+
+def tearDownModule():
+    print("tearing down module")
+
+
+class TestClass1(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('John Smith'.split(), ['John', 'Smith'])
+
+
+class TestClass2(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('John Smith'.split(), ['John', 'Smith'])
+
+
+class TestClass3(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('John Smith'.split(), ['John', 'Smith'])
