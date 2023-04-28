ea74033212eab309dbec1e066edfbcdca1f84df3
Oliwier Kossak <oliwierkossak@wp.pl>
2023-04-28 17:39:58 +0200

    unittest skip

---
+++ 04_skiping_tests/skip_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestClass(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual('aws'.upper(),'AWS')
+
+    @unittest.skip('test skipping')
+    def test_case_2(self):
+        self.assertEqual('aws'.upper(),'AWS')
